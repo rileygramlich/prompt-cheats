43974f7c7475a6ffba2412fbb6e6b6ae89a4cc1c
Riley Gramlich <gramlichriley@gmail.com>
2023-01-16 14:33:41 -0700

    complete part 5

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,31 @@
+const User = require("../../models/user");
+const jwt = require('jsonwebtoken')
+
+
 module.exports = {
     create
   };
+
+  /*-- Helper Functions --*/
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
+}
+
   
-  function create(req, res) {
-    // Baby step...
-    res.json({
-      user: {
-        name: req.body.name,
-        email: req.body.email
-      }
-    });
+  async function create(req, res) {
+   try {
+    const user = await User.create(req.body);
+    const token = createJWT(user)
+    res.status(200).json(token)
+   } catch (err) {
+    console.log(err)
+    res.status(400).json(err)
+   }
   }
+
