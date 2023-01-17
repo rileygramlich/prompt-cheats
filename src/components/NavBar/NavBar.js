4ab3793fcf7b5b87548da9a3480abd0bf9d70353
Riley Gramlich <101842921+rileygramlich@users.noreply.github.com>
2023-01-17 09:10:56 -0700

    save user toekn in local storage to validate user annd render user infor to page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ name }) {
   return (
     <nav>
-        <Link to="/home/orders" >Order History</Link>
-        &nbsp; | &nbsp;
-        <Link to="/home/new">New Order</Link>
-        </nav>
-  )
+      <Link to="/home/orders">Order History</Link>
+      &nbsp; | &nbsp;
+      <Link to="/home/new">New Order</Link>
+      <div className="welcome">Welcome {name}!</div>
+    </nav>
+  );
 }
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { getUser } from '../../utilities/users-service';
 
 import Home from "../Home/Home";
 import AuthPage from "../AuthPage/AuthPage";
@@ -9,14 +10,15 @@ import NavBar from "../../components/NavBar/NavBar";
 import Number from "../../components/Number/Number";
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
+  const name = user.name
 
   return (
     <main className="App">
       <h1 className="hello">React is amazing.</h1>
       {user ? (
         <>
-          <NavBar />
+          <NavBar name={name}/>
           <Routes>
             <Route path="/home/new" element={<Home />} />
             <Route path="/home/orders" element={<OrderHistoryPage />} />
